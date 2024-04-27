1c84a409a659496ec9179b808eb3446310b1a78e
可乐 <zaxlct@foxmail.com>
2024-04-27 20:42:54 +0800

    add demo

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
-import { createPinia } from 'pinia';
-import { createApp } from 'vue';
-import router from './router';
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import formCreate from '@form-create/ant-design-vue'
+import install from '@form-create/ant-design-vue/auto-import'
+import router from './router'
 
-import directives from '@/directives';
-import plugins from '@/plugins';
-import '@/assets/styles/common.less';
+import directives from '@/directives'
+import plugins from '@/plugins'
+import '@/assets/styles/common.less'
+import App from './App.vue'
+import { authPlugin } from '@bwrong/auth-tool'
+formCreate.use(install)
 
-import App from './App.vue';
-import { authPlugin } from '@bwrong/auth-tool';
-
-const app = createApp(App);
-app.use(createPinia());
-app.use(router);
-app.use(plugins); // 注册插件
-app.use(directives); // 注册指令
-app.use(authPlugin);
-app.mount('#app');
-export default app;
+const app = createApp(App)
+app.use(createPinia())
+app.use(formCreate)
+app.use(router)
+app.use(plugins) // 注册插件
+app.use(directives) // 注册指令
+app.use(authPlugin)
+app.mount('#app')
+export default app
--- src/views/home/demo.vue
+++ src/views/home/demo.vue
@@ -1,3 +1,56 @@
 <template>
-  <h1>22</h1>
+  <div>
+    <p>formData: {{ value }}</p>
+    <form-create
+      :rule="rule"
+      v-model:api="fApi"
+      :option="options"
+      v-model="value"
+    >
+      <template #type-btns>
+        <ACol
+          :push="3"
+          style="margin-bottom: 20px;"
+        >
+          <AButton @click="changeValue">修改 field1</AButton>
+          <AButton
+            @click="changeForm"
+            style="margin-left:20px;"
+          >修改表单</AButton>
+        </ACol>
+      </template>
+    </form-create>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      fApi: {},
+      value: { field1: '111', field2: '222', date: '2023-10-23' },
+      options: {
+        onSubmit: (formData) => {
+          alert(JSON.stringify(formData))
+        },
+        resetBtn: true
+      },
+      rule: [
+        { type: 'input', field: 'field1', title: 'field1', value: '' },
+        { type: 'input', field: 'field2', title: 'field2', value: '' },
+        { type: 'datePicker', field: 'date', title: 'date', value: '' },
+        { type: 'btns' },
+
+      ]
+    }
+  },
+  methods: {
+    changeValue() {
+      this.value.field1 += '-a'
+    },
+    changeForm() {
+      this.value = { field1: '666', field2: '666', date: '2023-09-23' }
+    }
+  }
+}
+</script>

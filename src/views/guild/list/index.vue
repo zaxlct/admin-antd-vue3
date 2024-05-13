0b036d182c86b524515f12a384e23156d89ab55a
可乐 <zaxlct@foxmail.com>
2024-05-13 11:30:11 +0800

    rename

--- src/views/guild/list/index.vue
+++ src/views/guild/list/index.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>guild</h1>
+  <div class="page_container">
+    <FormSearch
+      ref="formSearchRef"
+      v-model="searchParams"
+      @addItem="() => customTableRef.editItem()"
+    />
+    <CustomTable
+      ref="customTableRef"
+      :searchParams="searchParams"
+      :resetSearch="() => formSearchRef.resetForm()"
+    />
+  </div>
+
 </template>
+
+<script setup lang="jsx">
+import CustomTable from './components/CustomTable.vue'
+import FormSearch from './components/FormSearch.vue'
+
+const customTableRef = ref(null)
+const formSearchRef = ref(null)
+const searchParams = ref({})
+</script>

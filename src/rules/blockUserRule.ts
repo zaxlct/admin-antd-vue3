8a8103099eb1b9f4cb72e18efd9fa88bdbdb0a08
可乐 <zaxlct@foxmail.com>
2024-05-16 14:50:28 +0800

    简化重复代码

---
+++ src/rules/blockUserRule.ts
@@ -0,0 +1,62 @@
+import dayjs from 'dayjs'
+
+export default [
+  {
+    type: 'radio',
+    field: 'block_type',
+    title: '拉黑类型',
+    value: '',
+    options: Object.keys(ENUM.block_type).map(key => ({ label: ENUM.block_type[key], value: parseInt(key) })),
+    effect: {
+      required: true
+    }
+  },
+  {
+    type: 'radio',
+    field: 'ageing_type',
+    title: '拉黑时效',
+    value: '',
+    options: Object.keys(ENUM.ageing_type).map(key => ({ label: ENUM.ageing_type[key], value: parseInt(key) })),
+    effect: {
+      required: true
+    },
+    control: [
+      {
+        handle: val => val === 3,
+        append: 'ageing_type',
+        rule: [
+          {
+            type: 'datePicker',
+            field: 'end_time',
+            title: '拉黑时间',
+            value: '',
+            effect: {
+              required: true
+            },
+            props: {
+              placeholder: '请选择时间',
+              showTime: { defaultValue: dayjs('00:00:00', 'HH:mm:ss') },
+              disabledDate: (current) => {
+                return current && current < dayjs().endOf('day')
+              },
+              format: 'YYYY-MM-DD HH:mm:ss',
+              valueFormat: 'X',
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    type: 'input',
+    field: 'reason',
+    title: '理由',
+    value: '',
+    props: {
+      type: 'textarea'
+    },
+    effect: {
+      required: true
+    },
+  },
+]
--- src/views/user/list/components/CustomTable.vue
+++ src/views/user/list/components/CustomTable.vue
@@ -22,9 +22,9 @@
 </template>
 
 <script setup lang="jsx">
-import dayjs from 'dayjs'
 import { getUserListReq, getUserLogListReq, setUserRemarkReq, setBlackReq, setMuteReq, setUserTagsReq, userAddOrEditReq, createUserIdReq, resetPasswordReq } from '@/api/users'
 import { getUserFunclubListReq } from '@/api/public'
+import blockUserRule from '@/rules/blockUserRule'
 
 const props = defineProps({
   searchParams: {
@@ -536,64 +536,7 @@ function blockUser(userItem) {
         value: userItem.user_id,
         hidden: true,
       },
-      {
-        type: 'radio',
-        field: 'block_type',
-        title: '拉黑类型',
-        value: '',
-        options: Object.keys(ENUM.block_type).map(key => ({ label: ENUM.block_type[key], value: parseInt(key) })),
-        effect: {
-          required: true
-        }
-      },
-      {
-        type: 'radio',
-        field: 'ageing_type',
-        title: '拉黑时效',
-        value: '',
-        options: Object.keys(ENUM.ageing_type).map(key => ({ label: ENUM.ageing_type[key], value: parseInt(key) })),
-        effect: {
-          required: true
-        },
-        control: [
-          {
-            handle: val => val === 3,
-            append: 'ageing_type',
-            rule: [
-              {
-                type: 'datePicker',
-                field: 'end_time',
-                title: '拉黑时间',
-                value: '',
-                effect: {
-                  required: true
-                },
-                props: {
-                  placeholder: '请选择时间',
-                  showTime: { defaultValue: dayjs('00:00:00', 'HH:mm:ss') },
-                  disabledDate: (current) => {
-                    return current && current < dayjs().endOf('day')
-                  },
-                  format: 'YYYY-MM-DD HH:mm:ss',
-                  valueFormat: 'X',
-                }
-              }
-            ]
-          }
-        ]
-      },
-      {
-        type: 'input',
-        field: 'reason',
-        title: '理由',
-        value: '',
-        props: {
-          type: 'textarea'
-        },
-        effect: {
-          required: true
-        },
-      },
+      ...blockUserRule,
     ],
   }
 

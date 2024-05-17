54af207ba1aae85bcf3c1771d49d22649529be0e
可乐 <zaxlct@foxmail.com>
2024-05-17 16:07:18 +0800

    推荐位

---
+++ src/views/recweight/list/components/CustomTable.vue
@@ -0,0 +1,269 @@
+<template>
+  <a-table
+    rowKey="guild_id"
+    :pagination="false"
+    :scroll="{ x: 1200, y: 800 }"
+    :dataSource
+    :columns="columns"
+    :loading="loading"
+  />
+  <a-pagination
+    class="mt15"
+    hideOnSinglePage
+    v-model:current="pagination.page"
+    v-model:pageSize="pagination.limit"
+    size="small"
+    :total="pagination.total"
+  />
+</template>
+
+<script setup lang="jsx">
+import { debounce } from 'lodash-es'
+import mitt from 'mitt'
+import dayjs from 'dayjs'
+import { getAnchorReweightListReq, recweightAddOrEditReq, getAnchorInfoReq } from '@/api/anchor'
+import ENUMS from '@/enums/common'
+
+const props = defineProps({
+  searchParams: {
+    type: Object,
+    default: () => ({}),
+  },
+  resetSearch: {
+    type: Function,
+    default: () => { },
+  },
+})
+
+const pagination = reactive({
+  page: 1,
+  limit: 10,
+  total: 0,
+})
+const dataSource = ref([])
+const { loading, refresh } = useRequest(() => getAnchorReweightListReq({
+  ...props.searchParams,
+  page: pagination.page,
+  limit: pagination.limit,
+}), {
+  refreshDeps: true,
+  onSuccess(data) {
+    dataSource.value = data.items
+    pagination.total = data.total_data
+  },
+})
+const { createDialog } = useDialog()
+
+const columns = [
+  {
+    title: '主播头像',
+    dataIndex: 'avatar_url',
+    customRender: ({ record }) =>
+      <a-avatar src={record.avatar_url} size={40} />
+  },
+  {
+    title: '主播昵称',
+    dataIndex: 'nickname',
+  },
+  {
+    title: '主播ID',
+    dataIndex: 'anchor_id',
+  },
+  {
+    title: '所属公会',
+    dataIndex: 'guild_rel',
+    customRender: ({ record }) => <div>{record.guild_rel?.guild_name}%</div>
+  },
+  {
+    title: '推荐权重',
+    dataIndex: 'rec_weight',
+    customRender: ({ record }) => <div>{record.rec_weight}</div>
+  },
+  {
+    title: '生效时间',
+    dataIndex: 'effect_time',
+    customRender: ({ record }) => <div v-if={record.effect_time}>{record.effect_time[0]}-{record.effect_time[1]}-</div>
+  },
+  {
+    title: '设置时间',
+    dataIndex: 'create_time',
+  },
+  {
+    title: '推荐状态',
+    dataIndex: 'rec_status',
+    customRender: ({ record }) =>
+      <a-tag color={record.rec_status === 1 ? 'green' : 'red'}>
+        {ENUMS.recweight_status[record.rec_status]}
+      </a-tag>
+  },
+  {
+    title: '操作',
+    fixed: 'right',
+    width: 120,
+    dataIndex: 'action',
+    customRender: ({ record }) =>
+      <div>
+        <a-button type="link" size="small" onClick={() => editItem(record)}>编辑</a-button>
+      </div>
+  }
+]
+
+// 推荐主播/修改推荐权重
+async function editItem(Item = {}) {
+  const formValue = ref({
+    rec_weight: Item.rec_weight,
+    anchor_id: Item.anchor_id?.toString(), // formCreate 不支持 number 类型
+    nickname: Item.nickname,
+    guild_name: Item.guild_rel?.guild_name,
+    // format('X')
+    effect_time: Item.effect_time?.length ? [dayjs(Item.effect_time[0]).format('X'), dayjs(Item.effect_time[1]).format('X')] : [],
+  })
+
+  const eventBus = mitt()
+
+  const isCreate = !Item.rec_id
+  const currentInfo = ref({})
+  const formModalProps = reactive({
+    request: data => recweightAddOrEditReq(Item.rec_id || currentInfo.value.rec_id, data),
+    getData(data) {
+      return {
+        rec_id: Item.rec_id || currentInfo.value.rec_id,
+        rec_type: 'TODO',
+        rec_weight: data.rec_weight,
+        anchor_id: parseInt(data.anchor_id),
+        nickname: data.nickname,
+        guild_rel: currentInfo.value.guild_rel,
+        effect_time: data.effect_time,
+      }
+    },
+    rule: [
+      {
+        type: 'input',
+        field: 'anchor_id',
+        title: '主播ID',
+        value: '',
+        validate: [
+          {
+            required: true,
+            pattern: '^[0-9]*$',
+            message: '请输入主播ID，ID为纯数字组成'
+          }
+        ],
+        props: {
+          type: 'digit',
+          disabled: !isCreate,
+        },
+        options: [],
+        effect: {
+          loadData: {
+            attr: 'labelOptions',
+            to: 'options'
+          },
+        },
+        on: {
+          change: debounce(e => {
+            const value = e.target.value.trim()
+            eventBus.emit('switchLoading')
+            getAnchorInfoReq({ anchor_id: value, rec_type: 'TODO' }).then(data => {
+              if (data?.nickname) {
+                currentInfo.value = data // 保存查询的结果
+                formValue.value.nickname = data.nickname
+                formValue.value.rec_weight = data.rec_weight
+                if (data.effect_time?.length) {
+                  formValue.value.effect_time = [dayjs(data.effect_time[0]).format('X'), dayjs(data.effect_time[1]).format('X')]
+                }
+                if (data.guild_rel?.guild_id) {
+                  formValue.value.guild_name = data.guild_rel.guild_name
+                }
+                // 清除校验状态
+                eventBus.emit('fApiHandle', fApi => fApi.clearValidateState())
+              } else {
+                $message.error(`ID ${value} 未查到对应主播，请重新输入`)
+                formValue.value.nickname = ''
+                formValue.value.guild_name = ''
+                currentInfo.value = {}
+              }
+            }).finally(() => {
+              eventBus.emit('switchLoading')
+            })
+          }, 500),
+        },
+      },
+      {
+        type: 'input',
+        field: 'nickname',
+        title: '主播昵称',
+        value: '',
+        props: {
+          disabled: true,
+        },
+        effect: {
+          required: true,
+        }
+      },
+      {
+        type: 'input',
+        field: 'guild_name',
+        title: '所属公会',
+        value: '',
+        props: {
+          disabled: true,
+        },
+        effect: {
+          required: true,
+        }
+      },
+      {
+        type: 'input-number',
+        field: 'rec_weight',
+        title: '推荐权重',
+        value: '',
+        props: {
+          max: 100,
+          min: 0,
+          step: 1,
+          precision: 0,
+        },
+        effect: {
+          required: true,
+        }
+      },
+      {
+        type: 'rangePicker',
+        field: 'effect_time',
+        title: '推荐时间',
+        value: '',
+        props: {
+          format: 'YYYY-MM-DD',
+          valueFormat: 'X',
+        },
+        effect: {
+          required: true,
+        }
+      },
+    ],
+  })
+
+  createDialog({
+    title: '推荐主播',
+    width: 500,
+    component:
+      <ModalForm
+        v-model={formValue.value}
+        {...formModalProps}
+        eventBus={eventBus}
+      >
+      </ModalForm>
+    ,
+    onConfirm() {
+      pagination.page = 1
+      pagination.total = 0
+      props.resetSearch()
+    },
+  })
+}
+
+defineExpose({
+  editItem,
+})
+</script>
---
+++ src/views/recweight/list/components/FormSearch.vue
@@ -0,0 +1,122 @@
+<template>
+  <a-card class="mb15">
+    <div class="__table_form_search_component">
+      <form-create
+        v-model:api="fApi"
+        v-model="data"
+        :option
+        :rule
+      >
+        <template #type-btns>
+          <section
+            class="flex mb15"
+            style="width: 100%;"
+          >
+            <AButton
+              @click="submitForm"
+              type="primary"
+            >查询</AButton>
+            <AButton
+              class="ml10"
+              @click="resetForm"
+            >重置</AButton>
+            <div class="flex1 flex_end">
+              <AButton
+                type="primary"
+                @click="emit('addItem')"
+              >添加工会</AButton>
+            </div>
+          </section>
+        </template>
+      </form-create>
+    </div>
+  </a-card>
+</template>
+
+<script setup>
+import merchSelectRule from '@/rules/merchSelectRule'
+const params = defineModel()
+const data = reactive({
+  merch_id: 0,
+  guild_name: '',
+  status: 0,
+  reg_time: 0,
+})
+
+const emit = defineEmits(['addItem', 'search'])
+const fApi = ref({})
+const option = {
+  resetBtn: false,
+  submitBtn: false,
+  global: {
+    '*': {
+      col: {
+        show: false,
+      },
+      wrap: {
+        labelCol: { span: 8 },
+      },
+    },
+  },
+}
+
+const rule = ref([
+  merchSelectRule,
+  {
+    type: 'input',
+    field: 'guild_name',
+    title: '工会名称',
+    value: '',
+  },
+  {
+    type: 'rangePicker',
+    field: 'reg_time',
+    title: '时间区间',
+    value: '',
+    props: {
+      format: 'YYYY-MM-DD',
+      valueFormat: 'X',
+    },
+  },
+  {
+    type: 'select',
+    field: 'acct_status',
+    title: '账号状态',
+    value: '',
+    options: Object.keys(ENUM.guild_status).map(key => ({ value: parseInt(key), label: ENUM.guild_status[key] })),
+  },
+
+  {
+    type: 'input',
+    field: 'au_id',
+    title: '应用ID/用户ID',
+    value: '',
+  },
+  { type: 'btns' },
+])
+
+function resetForm() {
+  fApi.value.resetFields()
+  getData(data)
+}
+
+function submitForm() {
+  fApi.value.submit(formData => {
+    getData(formData)
+  })
+}
+
+function getData(data) {
+  params.value = {
+    ...data,
+    reg_time: data.reg_time ? data.reg_time?.join(',') : undefined,
+  }
+}
+
+defineExpose({
+  resetForm,
+})
+</script>
+
+<style lang="sass scoped">
+</style>

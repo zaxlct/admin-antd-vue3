ad45e017cd06e1d58e800a808cd405ebb135040d
可乐 <zaxlct@foxmail.com>
2024-05-17 16:47:22 +0800

    use jsx

--- src/views/recweight/list/index.vue
+++ src/views/recweight/list/index.vue
@@ -1,24 +1,39 @@
 <template>
-  <div class="page_container">
-    <FormSearch
-      ref="formSearchRef"
-      v-model="searchParams"
-      @addItem="() => customTableRef.editItem()"
-    />
-    <CustomTable
-      ref="customTableRef"
-      :searchParams="searchParams"
-      :resetSearch="() => formSearchRef.resetForm()"
-    />
-  </div>
-
+  <recweightComponent></recweightComponent>
 </template>
 
 <script setup lang="jsx">
 import CustomTable from './components/CustomTable.vue'
 import FormSearch from './components/FormSearch.vue'
 
-const customTableRef = ref(null)
-const formSearchRef = ref(null)
-const searchParams = ref({})
+const recweightComponent = defineComponent({
+  setup() {
+    const customTableRef = ref(null)
+    const formSearchRef = ref(null)
+    const searchParams = ref({})
+
+    return () => (
+      <div class="page_container">
+        <FormSearch
+          ref={el => formSearchRef.value = el}
+          v-model={searchParams.value}
+          v-on={
+            {
+              addItem: () => customTableRef.value.editItem(),
+            }
+          }
+        />
+        <CustomTable
+          ref={el => customTableRef.value = el}
+          searchParams={searchParams.value}
+          v-on={
+            {
+              resetSearch: () => formSearchRef.value.resetForm(),
+            }
+          }
+        />
+      </div>
+    )
+  },
+})
 </script>

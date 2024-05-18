d7f1860d0b0614f387d9861194a5b389b8b97b9d
可乐 <zaxlct@foxmail.com>
2024-05-18 17:26:16 +0800

    初始化路由

---
+++ src/views/gift/list/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page_container">
+    <FormSearch
+      ref="formSearchRef"
+      v-model="searchParams"
+      @addItem="() => customTableRef.editItem()"
+    />
+    <CustomTable
+      ref="customTableRef"
+      :searchParams="searchParams"
+      :resetSearch="() => formSearchRef.resetForm()"
+    />
+  </div>
+
+</template>
+
+<script setup lang="jsx">
+import CustomTable from './components/CustomTable.vue'
+import FormSearch from './components/FormSearch.vue'
+
+const customTableRef = ref(null)
+const formSearchRef = ref(null)
+const searchParams = ref({})
+</script>

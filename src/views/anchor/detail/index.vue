50f886709b76e2eef9a4bf2ec31922c1c8506473
可乐 <zaxlct@foxmail.com>
2024-05-18 17:26:22 +0800

    主播管理

--- src/views/anchor/detail/index.vue
+++ src/views/anchor/detail/index.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>anchor detail</h1>
+  <a-card title="主播详情">
+    <form-create
+      v-model:api="fApi"
+      v-model="value"
+      :option="formCreateOptions"
+      :rule
+    >
+    </form-create>
+  </a-card>
 </template>
+
+<script setup lang="jsx">
+import useAnchorRule from '@/views/anchor/list/hooks/useAnchorRule'
+
+const anchorRule = useAnchorRule()
+const fApi = ref(null)
+const rule = ref([
+  ...anchorRule,
+])
+const formCreateOptions = {
+  submitBtn: false,
+  resetBtn: false,
+}
+
+</script>
